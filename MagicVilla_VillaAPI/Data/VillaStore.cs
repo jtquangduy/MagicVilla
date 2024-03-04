01d7f42b5d92ab9b93590ea6ed5833b3feea65a2
Quang Duy <justinquangduy@gmail.com>
2024-03-04 13:37:59 +0700

    Create Villa Table

--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -5,8 +5,8 @@ namespace MagicVilla_VillaAPI.Data
     public class VillaStore
     {
         public static List<VillaDTO> villaList = new List<VillaDTO> {
-                 new VillaDTO{Id=1,Name="Pool View",Sqrt=100,Occupancy=4},
-                 new VillaDTO{Id=2,Name="Beach View",Sqrt=300,Occupancy=3}
+                 new VillaDTO{Id=1,Name="Pool View",Sqft=100,Occupancy=4},
+                 new VillaDTO{Id=2,Name="Beach View",Sqft=300,Occupancy=3}
             };
     }
 }
