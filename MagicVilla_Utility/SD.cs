65379c03e84b88af59ac92a1ebf451c9480781d5
Quang Duy <justinquangduy@gmail.com>
2024-03-13 12:57:04 +0700

    Auth Controller Action Methods

--- MagicVilla_Utility/SD.cs
+++ MagicVilla_Utility/SD.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MagicVilla_Utility
+﻿namespace MagicVilla_Utility
 {
     public static class SD
     {
         public enum ApiType
         {
-            GET, 
-            POST, 
-            PUT, 
+            GET,
+            POST,
+            PUT,
             DELETE
         }
+
+        public static string SessionToken = "JWTToken";
     }
 }
--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -18,6 +18,13 @@ builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -34,6 +41,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
