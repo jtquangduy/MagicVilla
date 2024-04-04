a5343aee8fa8ef1b314d4e5af234a7b58ecf5bf8
Quang Duy <justinquangduy@gmail.com>
2024-04-04 19:09:12 +0700

    Show Roles in Dropdown on Register

--- MagicVilla_Utility/SD.cs
+++ MagicVilla_Utility/SD.cs
@@ -12,5 +12,7 @@
 
         public static string SessionToken = "JWTToken";
         public static string CurrentAPIVersion = "v2";
+        public const string Admin = "admin";
+        public const string Customer = "customer";
     }
 }
