b535929170eea9491dc44d236452cd1a54946784
Quang Duy <justinquangduy@gmail.com>
2024-04-22 17:00:44 +0700

    Content Type for FormFile

--- MagicVilla_Web/Services/VillaService.cs
+++ MagicVilla_Web/Services/VillaService.cs
@@ -23,7 +23,8 @@ namespace MagicVilla_Web.Services
                 ApiType = SD.ApiType.POST,
                 Data = dto,
                 Url = villaUrl + $"/api/{SD.CurrentAPIVersion}/villaAPI",
-                Token = token
+                Token = token,
+                ContentType = SD.ContentType.MultipartFormData
             });
         }
 
@@ -64,7 +65,8 @@ namespace MagicVilla_Web.Services
                 ApiType = SD.ApiType.PUT,
                 Data = dto,
                 Url = villaUrl + $"/api/{SD.CurrentAPIVersion}/villaAPI/" + dto.Id,
-                Token = token
+                Token = token,
+                ContentType = SD.ContentType.MultipartFormData
             });
         }
     }
